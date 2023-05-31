ba4ad7f9385a5e9441c260a4b21ef063699bc885
Olga Chernega <eliang.cheng@gmail.com>
2023-05-31 15:11:30 +0300

    init: add project config files

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css';
+import { Nunito } from 'next/font/google';
+
+const font = Nunito({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone with Next.js',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={font.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  );
+}
